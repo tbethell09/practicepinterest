01f4b424ad22b409b5a87c29a8af28d72d7a5581
Tom Bethell <tbethell09@gmail.com>
2015-03-14 15:21:23 +0000

    set up devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #required for heroku
+  config.action_mailer.default_url_options = { host: 'omr-sporterest.com'}
+
+  
 end
