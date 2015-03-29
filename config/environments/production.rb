631b9274fe9204538d3fc8313c84eac2f3483deb
Tom Bethell <tbethell09@gmail.com>
2015-03-29 23:02:55 +0000

    amazon s3 to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,17 @@ Rails.application.configure do
   
   #required for heroku
   config.action_mailer.default_url_options = { host: 'omr-sporterest.com'}
+  
+  
+      #sets paperclip to uplaod images to aws s3
+      config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['S3_BUCKET_NAME'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
 
   
 end
